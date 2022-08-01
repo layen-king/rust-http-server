a5a033995656107e5aa32594608e9fd10889837c
layen-king <446495695@qq.com>
2022-08-01 16:47:45 +0800

    feat: current_dir

--- src/utils/file.rs
+++ src/utils/file.rs
@@ -4,8 +4,13 @@ use std::io::{Read, Result};
 
 /// 获取文件内容
 pub fn get_file_contents(path: &str) -> Result<String> {
-    let file = File::open(path)?;
-    println!("file:{:?}",file);
+    // let current_dir = std::env::current_dir()?;
+    // let current_path = current_dir.join(path);
+    // println!("current_path:{:?}", current_path);
+    let current_path = format!("{}{}",std::env::current_dir().unwrap().display(), path);
+    println!("current_path:{:?}", current_path);
+    let file = File::open(current_path)?;
+    println!("file:{:?}", file);
     let mut buf_reader = BufReader::new(file);
     let mut contents = String::new();
     buf_reader.read_to_string(&mut contents)?;
